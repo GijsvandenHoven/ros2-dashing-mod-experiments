72a7ad41901b7e1c35ad793e27e0c39d0cb46477
Ruben de Voogd <r.j.d.voogd@student.tue.nl>
2022-06-27 17:55:00 +0200

    jitter

--- ros1/src/basic_bench/src/config.h
+++ ros1/src/basic_bench/src/config.h
@@ -10,7 +10,7 @@
 #define BASIC_BENCH_PUBLISHER_EXTRA 100
 
 // toggle for setting a transport hint in the subscriber.
-#define USE_TCP_HINT
+//#define USE_TCP_HINT
 // toggle for std::chrono use. The VM seems to not support this for ROS1, need to update the compiler, but want to have the code ready.
 // #define USE_CHRONO
 
